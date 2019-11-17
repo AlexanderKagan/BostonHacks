1066bc94d86f4e782bcc785c9430998c50e7077a
max_shaposhnikov <chelseamax@yandex.ru>
2019-11-17 09:38:40 +0500

    basic recorder

---
+++ server.py
@@ -0,0 +1,26 @@
+import json
+
+import bottle
+from bottle import route, request, error, template, static_file
+
+app = bottle.app()
+
+
+@route('/')
+def index():
+    return template('static/index.html')
+
+
+@route('/recorder_receiver', method='POST')
+def recorder_receiver():
+    audio_data = request.files.get('audio_data')
+    audio_data.save('record.wav')
+
+@route('/<filename:path>')
+def send_file(filename):
+    return static_file(filename, root='static/')
+
+
+if __name__ == '__main__':
+    bottle.debug(True)
+    bottle.run(app=app, host='localhost', port=5050)
